b73d24ffdaea4b14842bb9f96192c891d392c9c8
madoci <madoci@debian-stretch.com>
2020-04-23 22:20:46 +0200

    Javadoc et commentaires des classes et méthodes actuelles

--- src/main/java/fr/uga/fran/CSVParser.java
+++ src/main/java/fr/uga/fran/CSVParser.java
@@ -6,36 +6,66 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * A CSV parser that can read and parse CSV files. 
+ * This class uses a Scanner to read the file.
+ * @author ANDRE Stephen
+ * @author FREBY Laura
+ * @version
+ * @since 0.2.0
+ */
 public class CSVParser {
-	Scanner scanner;
-	int lineNumber;
+	Scanner scanner;	// Scanner to read the file
+	int lineNumber;		// Current line number for debug only
 	
-	public CSVParser(String fileName) throws FileNotFoundException {
-		scanner = new Scanner(new File(fileName));
+	/**
+	 * Constructs a CSVParser from the specified file. 
+	 * @param pathname	the path of the CSV file to parse
+	 * @throws FileNotFoundException if the file at pathname is not found
+	 */
+	public CSVParser(String pathname) throws FileNotFoundException {
+		scanner = new Scanner(new File(pathname));
 		lineNumber = 0;
 	}
 	
+	/**
+	 * Parses the current line of the CSV file, and advances to the next line. 
+	 * First call of this method will parse the first line of the file.
+	 * @return an array of all data read on the current line, or null if
+	 * @throws InvalidCSVFormatException if the file misses a closing double quote on a line
+	 */
 	public Object[] readLine() throws InvalidCSVFormatException {
+		// If the scanner is at the end of the file, return null
 		if (!scanner.hasNextLine()) {
 			return null;
 		}
 		
+		// Retrieves current line and advances the scanner to the next line
 		String line = scanner.nextLine();
 		lineNumber++;
 		
 		List<Object> objects = new ArrayList<>();
 		String data = "";
 		int i = 0;
+		
+		// Read the line character by character
 		while (i < line.length()) {
 			switch (line.charAt(i)) {
 			case ',':
+				// If we find a comma, we convert the current data into the correct type,
+				// add it to the list and reset the data to an empty string
 				objects.add(processData(data));
 				data = "";
 				break;
 			case '"':
+				// If we find an opening double quote, all characters until the next closing 
+				// double quote will be part of the data.
 				i++;
 				while (i < line.length()) {
 					if (line.charAt(i) == '"') {
+						// If a double quote is directly followed by another one, it is
+						// interpreted as the double quote character.
+						// Otherwise, it is a closing double quote.
 						if (i+1 < line.length() && line.charAt(i+1) == '"') {
 							i++;
 						} else {
@@ -45,6 +75,8 @@ public class CSVParser {
 					data += line.charAt(i);
 					i++;
 				}
+				// If we're at the end of the line and no closing double quote has been found,
+				// the format of the CSV file is invalid.
 				if (i >= line.length()) {
 					throw new InvalidCSVFormatException("missing '\"' at line " + lineNumber);
 				}
@@ -55,22 +87,30 @@ public class CSVParser {
 			}
 			i++;
 		}
+		
+		// Add the remaining data to the list
 		objects.add(processData(data));
 		
 		return objects.toArray();
 	}
 	
+	
+	/** Private methods **/
+	
+	// Convert a String object into the interpreted type (Integer, Double or String)
 	private Object processData(String data) {
 		Object obj = null;
 		
 		if (data.length() > 0) {
-			// Reconnait les champs de type String, Integer ou Double
+			// We first try to convert it into an Integer
 			try {
 				obj = Integer.valueOf(data);
 			} catch (Exception notInt) {
+				// If it fails, we then try to convert it into a Double
 				try {
 					obj = Double.valueOf(data);
 				} catch (Exception notDouble) {
+					// If it fails again, we keep it as a String
 					obj = data;
 				}
 			}
--- src/main/java/fr/uga/fran/Dataframe.java
+++ src/main/java/fr/uga/fran/Dataframe.java
@@ -4,7 +4,17 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A two-dimensional table with heterogeneous data types. 
+ * Each column is labeled with a name and contains data of a single type.
+ * Two columns of the same Dataframe can contain data of different types.
+ * @author ANDRE Stephen
+ * @author FREBY Laura
+ * @version 
+ * @since 0.1.0
+ */
 public class Dataframe {
+	// Class for the labeled columns inside a Dataframe
 	private class Column {
 		private final Class<?> type;
 		private String label;
@@ -27,26 +37,33 @@ public class Dataframe {
 	
 	private List<Column> columns;
 	
-	/** Public methods **/
-	
+	/**
+	 * Constructs a Dataframe from an array of labels and arrays of columns. 
+	 * @param labels	the array of labels, in the same order as the columns
+	 * @param data		variable amount of arrays each containing the content of a column
+	 */
 	public Dataframe(String labels[], Object[] ...data) {
 		columns = new ArrayList<>();
 		int numLines = 0;
 		
-		// Ajout des colonnes
+		// Add columns
 		for (int i=0; i<data.length; i++) {
+			// If no label is provided, the label is an empty string
 			String label = "";
 			if (i < labels.length) {
 				label = labels[i];
 			}
+			
+			// The data type of the column is retrieved from the first element of this column
 			addColumn(data[i][0].getClass(), label);
 			
+			// Keep track of the maximum number of lines between all columns
 			if (data[i].length > numLines) {
 				numLines = data[i].length;
 			}
 		}
 		
-		// Ajout des lignes
+		// Add lines
 		for (int i=0; i<numLines; i++) {
 			Object line[] = new Object[columns.size()];
 			for (int j=0; j<columns.size(); j++) {
@@ -60,11 +77,19 @@ public class Dataframe {
 		}
 	}
 	
-	public Dataframe(String CSVFileName) throws FileNotFoundException, InvalidCSVFormatException {
+	/**
+	 * Constructs a Dataframe from a CSV file. 
+	 * @param pathname	pathname of the CSV file to use
+	 * @throws FileNotFoundException	if the file at pathname is not found
+	 * @throws InvalidCSVFormatException	if the file does not follow CSV format
+	 * @since 0.2.0
+	 */
+	public Dataframe(String pathname) throws FileNotFoundException, InvalidCSVFormatException {
 		columns = new ArrayList<>();
 		
-		CSVParser parser = new CSVParser(CSVFileName);
+		CSVParser parser = new CSVParser(pathname);
 		
+		// First line of the file contains the labels
 		Object[] labels = parser.readLine();
 		if (labels == null) {
 			throw new InvalidCSVFormatException("file is empty");
@@ -72,19 +97,24 @@ public class Dataframe {
 		
 		List<Object[]> lines = new ArrayList<>();
 		Object[] data;
+		
+		// Parse and store all lines first
 		while ((data = parser.readLine()) != null) {
 			lines.add(data);
+			// All lines must have the same number of fields
 			if (data.length != labels.length) {
 				throw new InvalidCSVFormatException("invalid number of fields at line " + lines.size());
 			}
 		}
 		
+		// Add columns
 		for (int i=0; i<labels.length; i++) {
-			// Cherche le type de la colonne
+			// Search first not null element in the column to retrieve the type
 			int j = 0;
 			while (j < lines.size() && lines.get(j)[i] == null) {
 				j++;
 			}
+			// Empty column is considered an error because we cannot infer data type
 			if (j >= lines.size()) {
 				throw new InvalidCSVFormatException("no data found in column " + i);
 			}
@@ -92,23 +122,44 @@ public class Dataframe {
 			addColumn(lines.get(j)[i].getClass(), (String) labels[i]);
 		}
 		
+		// Add lines
 		for (Object[] line : lines) {
 			addLine(line);
 		}
 	}
 	
+	/**
+	 * Access a single element from a pair of line/column indexes. 
+	 * @param line	the line index 
+	 * @param column	the column index
+	 * @return	the element located at indexes (line, column)
+	 */
 	public Object get(int line, int column) {
 		return columns.get(column).get(line);
 	}
 	
+	/**
+	 * Access the label of a column. 
+	 * @param column	the column index
+	 * @return	the label of the column at the given index
+	 */
 	public String getLabel(int column) {
 		return columns.get(column).getLabel();
 	}
 	
+	/**
+	 * Access the data type of a column from the column index. 
+	 * @param column	the column index
+	 * @return	the data type of the column at the given index
+	 */
 	public Class<?> getType(int column) {
 		return columns.get(column).getType();
 	}
 	
+	/**
+	 * Add a line of data to this Dataframe. 
+	 * @param line	the array of line data to add
+	 */
 	public void addLine(Object line[]) {
 		for (int i=0; i<columns.size(); i++) {
 			if (i < line.length) {
@@ -119,8 +170,10 @@ public class Dataframe {
 		}
 	}
 	
+	
 	/** Private methods **/
 	
+	// Add to this Dataframe a labeled column of given data type
 	private void addColumn(Class<?> type, String label) {
 		columns.add(new Column(type, label));
 	}

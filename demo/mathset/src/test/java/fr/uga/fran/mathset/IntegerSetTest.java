212d132fcd70607a2fc2e48684def8a8f06d0338
madoci <madoci@debian-stretch.com>
2020-04-28 16:19:00 +0200

    Teste l'union et l'intersection sur des ensembles finis

--- demo/mathset/src/test/java/fr/uga/fran/mathset/IntegerSetTest.java
+++ demo/mathset/src/test/java/fr/uga/fran/mathset/IntegerSetTest.java
@@ -7,8 +7,35 @@ import org.junit.Test;
 public class IntegerSetTest {
 
 	@Test
-	public void testUnion() {
-		fail("Not yet implemented");
+	public void testNotInfinite() {
+		Integer i[] = { 1, 2, 6, 0, -3, 7 };
+		Integer j[] = { 4, 1, 7, 3 };
+		IntegerSet a = new IntegerSet(i);
+		IntegerSet b = new IntegerSet(j);
+		
+		IntegerSet union = a.union(b);
+		
+		assertTrue(union.contains(1));
+		assertTrue(union.contains(2));
+		assertTrue(union.contains(3));
+		assertTrue(union.contains(4));
+		assertTrue(union.contains(6));
+		assertTrue(union.contains(7));
+		assertTrue(union.contains(-3));
+		assertFalse(union.contains(5));
+		assertFalse(union.contains(8));
+		
+		IntegerSet inter = a.intersection(b);
+		
+		assertTrue(inter.contains(1));
+		assertTrue(inter.contains(7));
+		assertFalse(inter.contains(2));
+		assertFalse(inter.contains(3));
+		assertFalse(inter.contains(4));
+		assertFalse(inter.contains(6));
+		assertFalse(inter.contains(-3));
+		assertFalse(inter.contains(5));
+		assertFalse(inter.contains(8));
 	}
 
 }
